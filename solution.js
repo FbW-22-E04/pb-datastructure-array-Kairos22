7000e73e7961b93a77281c7d15d83a907cc6caeb
Kairos22 <alandiko84@gmail.com>
2022-07-07 12:57:25 +0200

    Add exercise 1

---
+++ solution.js
@@ -0,0 +1,15 @@
+// 1
+const myArray = ['egg', 'fish', 'milk','bread', 'cheese']
+console.log(myArray);
+console.log('--------------------------------');
+
+// 2
+myArray [2] = 'milk'
+const thirdElement = myArray[2];
+console.log(thirdElement);
+console.log('--------------------------------');
+
+// 3
+myArray[3] = 'fourth element'
+console.log(myArray);
+
